09f55c0646486f2b9de7b585841a2d8763685902
alemas1597 <alemas1597@gmail.com>
2023-01-28 14:34:07 -0600

    casi

--- todo/todou/todo3_cons.py
+++ todo/todou/todo3_cons.py
@@ -7,7 +7,6 @@ from todo.db import get_db
 
 bp = Blueprint('todo3_cons', __name__)
 
-#Citas
 @bp.route('/paciente/expediente', methods=['GET', 'POST'])
 @login_required
 def view_expediente():
@@ -16,16 +15,43 @@ def view_expediente():
      
     db, c = get_db()
     c.execute(
-        'select c.id_consulta, t.tx_nombre, '
-        'c.fk_id_paciente, '
-        'd.id_pago, d.nu_total '
-        'from consulta c JOIN persona p on c.fk_id_paciente = p.fk_id_usuario '
-        'JOIN usuarios u on u.id_usuario = c.fk_id_paciente '
+        'select c.id_consulta, t.tx_nombre, p.id_pago, p.nu_total ,i.fh_cita from consulta c JOIN citas i on i.id_cita = c.id_consulta '
         'JOIN tratamientos t on t.id_tratamiento =  c.fk_id_tratamiento '
-        'JOIN pagos d on d.id_pago = c.id_consulta '
-        'where u.id_usuario= %s order by u.tx_correo asc', 
+        'JOIN pagos p on p.fk_id_paciente = c.fk_id_paciente '
+        'where i.fk_id_paciente = %s', 
         (g.user['id_usuario'],)     
     )
     consultas = c.fetchall() 
 
     return render_template('todo3/view_cons.html', consultas = consultas)
+
+def get_pago(id_pago):
+     db, c = get_db()
+     c.execute(
+          'select * from pagos where id_pago = %s', 
+          (id_pago,)
+     )
+     pago = c.fetchone()
+
+     if pago is None:
+          abort(404, "El ID del pago {0} no existe ".format(id_pago))
+        
+     return pago
+
+@bp.route('/<int:id_pago>/paciente/view/pago', methods=['GET', 'POST'])
+@login_required
+def view_pagos(id_pago):
+    pago = get_pago(id_pago)
+
+    db, c = get_db()
+    c.execute(
+        'select a.nu_nabono, a.nu_monto, a.fh_abono, p.nu_total ' 
+        'from abono a JOIN pagos p on p.id_pago = a.fk_id_pago ' 
+        'where fk_id_pago = %s'
+        ,(id_pago,)
+    )
+    pagos = c.fetchall()
+
+
+
+    return render_template('todo3/view_pago.html', pagos = pagos)
