a921389077089f60ac5e775592a082df183cd7c2
elias <braulioelias6@gmail.com>
2023-01-28 20:00:11 -0600

    style: cambio de dormatos

---
+++ start.sh
@@ -0,0 +1,9 @@
+export FLASK_DATABASE_HOST='localhost'
+export FLASK_DATABASE_USER='root'
+export FLASK_DATABASE='prueba'
+export FLASK_DATABASE_PASSWORD='1234'
+export FLASK_ENV=development
+export FLASK_APP=todo
+
+
+flask run
